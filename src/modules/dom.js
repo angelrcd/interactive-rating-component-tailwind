a54b297f69c237d47a31736e992e4ed4bafd4d28
Angel Romero <mrangelrcd@gmail.com>
2022-10-19 20:40:19 +0200

    Añadido estilo botones

---
+++ src/modules/dom.js
@@ -0,0 +1,20 @@
+const buttonSelectors = document.querySelectorAll(".lista-botones button");
+
+function removeAllSelectedButtons() {
+  buttonSelectors.forEach(button => {
+    button.classList.remove("bg-orange-500", "text-white", "ring-2", "ring-white");
+    button.classList.add("bg-gray-700", "text-gray-400", "hover:bg-gray-500");
+  });
+}
+
+function addButtonSelectedStyle(button) {
+  button.classList.remove("bg-gray-700", "text-gray-400", "hover:bg-gray-500");
+  button.classList.add("bg-orange-500", "text-white", "ring-2", "ring-white");
+}
+
+buttonSelectors.forEach(button => {
+  button.addEventListener("click", () => {
+    removeAllSelectedButtons();
+    addButtonSelectedStyle(button);
+  });
+});
